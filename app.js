49a9b2f7ef13b246e901d0d23095adac7f2b22a9
durdona <123@.ru>
2023-09-22 14:49:01 +0500

    final

---
+++ app.js
@@ -0,0 +1,32 @@
+/* // Задание 1
+// let number = +prompt("Введите число")
+// let step = +prompt("Введите степень")
+// let answer = 1
+
+// for (let i = 0; i <step; i++) {
+
+//     answer = number*answer
+    
+// }
+
+// alert( ' Ответ ' + answer ) */
+
+// Задание 2
+
+// let quantity = +prompt('Введите кол-во ступенек')
+// let symbol = prompt('Введите символ отступов')
+// let endsymbol = prompt('Введите конечный символ')
+// let aw = ''
+
+// for (let i = 0; i < quantity; i++) {
+   
+// if (i==0) {
+//    aw = endsymbol
+// }else{
+
+// aw = symbol + aw
+
+// }
+// console.log(aw);
+// }
+
